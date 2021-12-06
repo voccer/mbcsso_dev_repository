54f28b9af23417fcba1d7affb083cd5036d8e936
voccer <ntduc250798@gmail.com>
2021-12-07 00:43:54 +0700

    change python ver

--- src/authorizer/push_docker.sh
+++ src/authorizer/push_docker.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 aws ecr get-login-password --region ap-northeast-1 | docker login --username AWS --password-stdin 465316005105.dkr.ecr.ap-northeast-1.amazonaws.com
-docker build -t samssobfd0c93c/authfunctiona88a6b13repo:authfunction-03572dcb4392-python3.8-v1  .
-docker tag samssobfd0c93c/authfunctiona88a6b13repo:authfunction-03572dcb4392-python3.8-v1  465316005105.dkr.ecr.ap-northeast-1.amazonaws.com/samssobfd0c93c/authfunctiona88a6b13repo:authfunction-03572dcb4392-python3.8-v1 
-docker push 465316005105.dkr.ecr.ap-northeast-1.amazonaws.com/samssobfd0c93c/authfunctiona88a6b13repo:authfunction-03572dcb4392-python3.8-v1 
+docker build -t samssobfd0c93c/authfunctiona88a6b13repo:authfunction-03572dcb4392-python3.9-v1  .
+docker tag samssobfd0c93c/authfunctiona88a6b13repo:authfunction-03572dcb4392-python3.9-v1  465316005105.dkr.ecr.ap-northeast-1.amazonaws.com/samssobfd0c93c/authfunctiona88a6b13repo:authfunction-03572dcb4392-python3.9-v1 
+docker push 465316005105.dkr.ecr.ap-northeast-1.amazonaws.com/samssobfd0c93c/authfunctiona88a6b13repo:authfunction-03572dcb4392-python3.9-v1 
 MY_FUNC=mbcsso_dev_AuthFunction
 aws lambda update-function-code --function-name $MY_FUNC --image-uri $(aws lambda get-function --function-name $MY_FUNC | jq -r '.Code.ImageUri')
